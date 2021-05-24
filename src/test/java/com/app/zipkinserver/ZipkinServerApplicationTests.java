1a1bc699cd43d6e42ad53d17ec0b6d28737c7823
Nagesh Mishra <nagesh3.13@gmail.com>
2021-05-24 19:01:55 +0530

    Add files via upload

---
+++ src/test/java/com/app/zipkinserver/ZipkinServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.app.zipkinserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinServerApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}
